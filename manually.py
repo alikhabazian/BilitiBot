1f9100ba109f60b0527ab5d4f82d0c9845bf4269
alikhabazian <khabaziana@gmail.com>
2023-09-17 22:18:36 +0330

    add negeen

--- manually.py
+++ manually.py
@@ -3,10 +3,10 @@ import pickle
 from Task import Task,os
 
 Tasks=[
-    Task(creator = '684630739',receivers = ['684630739','1952566514'],how_often = 'daily',orgin_city = 'تهران',destination_city = 'اصفهان',date = '2023-09-20',start_time = '20:00:00',end_time = '23:59:59',),
-    Task(creator='684630739', receivers=['684630739','1952566514'], how_often='daily', orgin_city='تهران', destination_city='اصفهان',date='2023-09-21', start_time='00:00:00', end_time='02:00:00', ),
-    Task(creator='684630739', receivers=['684630739','1952566514'], how_often='daily', orgin_city='اصفهان', destination_city='تهران', date='2023-09-22', start_time='20:00:00', end_time='23:59:59', ),
-    Task(creator='684630739', receivers=['684630739','1952566514'], how_often='daily', orgin_city='اصفهان', destination_city='تهران',date='2023-09-23', start_time='00:00:00', end_time='02:00:00', ),
+    Task(creator = '684630739',receivers = ['684630739','1952566514','229091667'],how_often = 'daily',orgin_city = 'تهران',destination_city = 'اصفهان',date = '2023-09-20',start_time = '20:00:00',end_time = '23:59:59',),
+    Task(creator='684630739', receivers=['684630739','1952566514','229091667'], how_often='daily', orgin_city='تهران', destination_city='اصفهان',date='2023-09-21', start_time='00:00:00', end_time='02:00:00', ),
+    Task(creator='684630739', receivers=['684630739','1952566514','229091667'], how_often='daily', orgin_city='اصفهان', destination_city='تهران', date='2023-09-22', start_time='20:00:00', end_time='23:59:59', ),
+    Task(creator='684630739', receivers=['684630739','1952566514','229091667'], how_often='daily', orgin_city='اصفهان', destination_city='تهران',date='2023-09-23', start_time='00:00:00', end_time='02:00:00', ),
 
 ]
 
