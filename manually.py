e8964c5b7b88f4b52eec7f848f88856d058a8db1
alikhabazian <khabaziana@gmail.com>
2023-10-02 13:50:26 +0330

    change manually

--- manually.py
+++ manually.py
@@ -3,10 +3,10 @@ import pickle
 from Task import Task,os
 
 Tasks=[
-    Task(creator = '684630739',receivers = ['684630739','229091667'],how_often = 'daily',orgin_city = 'تهران',destination_city = 'اصفهان',date = '2023-09-27',start_time = '21:00:00',end_time = '23:59:59',),
-    Task(creator='684630739', receivers=['684630739', '229091667'], how_often='daily', orgin_city='تهران',destination_city='اصفهان', date='2023-09-28', start_time='00:00:00', end_time='02:00:00', ),
-    Task(creator='684630739', receivers=['684630739', '229091667'], how_often='daily', orgin_city='اصفهان',destination_city='تهران', date='2023-09-28', start_time='22:00:00', end_time='23:59:59', ),
-    Task(creator='684630739', receivers=['684630739', '229091667'], how_often='daily', orgin_city='اصفهان',destination_city='تهران', date='2023-09-29', start_time='00:00:00', end_time='05:00:00', ),
+    Task(creator = '684630739',receivers = ['684630739','229091667'],how_often = 'daily',orgin_city = 'تهران',destination_city = 'اصفهان',date = '2023-10-02',start_time = '21:00:00',end_time = '23:59:59',),
+    Task(creator='684630739', receivers=['684630739', '229091667'], how_often='daily', orgin_city='تهران',destination_city='اصفهان', date='2023-10-03', start_time='00:00:00', end_time='10:00:00', ),
+    Task(creator='684630739', receivers=['684630739', '229091667'], how_often='daily', orgin_city='اصفهان',destination_city='تهران', date='2023-10-06', start_time='20:00:00', end_time='23:59:59', ),
+    Task(creator='684630739', receivers=['684630739', '229091667'], how_often='daily', orgin_city='اصفهان',destination_city='تهران', date='2023-10-07', start_time='00:00:00', end_time='05:00:00', ),
 
 ]
 
